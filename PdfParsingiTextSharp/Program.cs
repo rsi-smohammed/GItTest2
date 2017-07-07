cc82a0d5479cf90e4d2dfe3c3c0fb8dc72e2a452
shoaibd <shaoib.mohammed@hmhco.com>
2017-07-07 17:07:25 -0500

    init check in for test proj

---
+++ PdfParsingiTextSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PdfParsingiTextSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var pp = @"C:\tempp\bb.pdf";
+            Console.WriteLine(ExtractTextFromPdf(pp));
+        }
+
+        public static string ExtractTextFromPdf(string path)
+        {
+
+            try
+            {
+                using (PdfReader reader = new PdfReader(path))
+                {
+                    StringBuilder text = new StringBuilder();
+
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                    }
+
+                    return text.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                var sdfsdfs = "";
+            }
+            return "";
+        }
+    }
+}
